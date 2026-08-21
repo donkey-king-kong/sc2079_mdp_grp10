300532758d71f352b10ebef7737e5815005db1ac
Carwyn Yeo Ruizhi <144221886+carwynyeo@users.noreply.github.com>
2026-08-21 11:28:02 +0900

    Delete app directory

--- app/src/main/java/com/example/sc2079/ui/bluetooth/DeviceItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sc2079.ui.bluetooth
-
-import android.bluetooth.BluetoothDevice
-
-data class DeviceItem(
-    val device: BluetoothDevice,
-    val isConnecting: Boolean = false,
-    val isConnected: Boolean = false
-)
--- app/src/main/java/com/example/sc2079/ui/coordinates/SharedViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.sc2079.ui.coordinates
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-class SharedViewModel : ViewModel() {
-    val newCoordinate = MutableLiveData<Pair<Float, Float>>()
-}
