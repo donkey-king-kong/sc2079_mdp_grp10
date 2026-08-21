7ef7d782410a5c336b8b68af106d294413646213
Carwyn Yeo Ruizhi <144221886+carwynyeo@users.noreply.github.com>
2026-08-21 11:27:23 +0900

    Delete RPI directory

--- RPI/bluetooth.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Bluetooth setup for Raspberry Pi
-sudo sdptool add --channel=2 SP
-# sudo systemctl start rfcomm
-sudo rfcomm release 0
-sudo pkill rfcomm
-sudo rfcomm listen /dev/rfcomm0 2
