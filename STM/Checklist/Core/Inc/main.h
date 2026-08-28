31c7fbe8c75754379c714529f9f75943ead62568
Ryan <tanr557@gmail.com>
2026-08-29 01:30:05 +0800

    Combined yixuan IMU sensor and ultrasonic sensor code.

--- STM/Checklist/Core/Inc/main.h
+++ STM/Checklist/Core/Inc/main.h
@@ -57,8 +57,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define ULTRASONIC_ECHO_Pin GPIO_PIN_2
+#define ULTRASONIC_ECHO_GPIO_Port GPIOA
+#define ULTRASONIC_TRIG_Pin GPIO_PIN_3
+#define ULTRASONIC_TRIG_GPIO_Port GPIOA
 #define LED3_Pin GPIO_PIN_8
 #define LED3_GPIO_Port GPIOE
+#define IMU_INT_Pin GPIO_PIN_12
+#define IMU_INT_GPIO_Port GPIOB
 #define OLED_DC_Pin GPIO_PIN_11
 #define OLED_DC_GPIO_Port GPIOD
 #define OLED_RESET__Pin GPIO_PIN_12
--- STM/Checklist/Core/Src/stm32f4xx_it.c
+++ STM/Checklist/Core/Src/stm32f4xx_it.c
@@ -55,6 +55,7 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim5;
 extern UART_HandleTypeDef huart3;
 extern TIM_HandleTypeDef htim7;
 
@@ -172,6 +173,20 @@ void USART3_IRQHandler(void)
   /* USER CODE BEGIN USART3_IRQn 1 */
 
   /* USER CODE END USART3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM5 global interrupt.
+  */
+void TIM5_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM5_IRQn 0 */
+
+  /* USER CODE END TIM5_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim5);
+  /* USER CODE BEGIN TIM5_IRQn 1 */
+
+  /* USER CODE END TIM5_IRQn 1 */
 }
 
 /**
