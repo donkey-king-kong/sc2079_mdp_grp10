b32aa28e2db6235053abd0cde09dcf838ca214f4
Ryan <tanr557@gmail.com>
2026-09-18 14:38:22 +0800

    - Duplicated checklist project to work on task 1. - Updated checklist main.c with cleaner code and completed checklist materials.

---
+++ STM/Task1/Core/Inc/main.h
@@ -0,0 +1,96 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define MOTOR_BIN1_Pin GPIO_PIN_5
+#define MOTOR_BIN1_GPIO_Port GPIOE
+#define MOTOR_BIN2_Pin GPIO_PIN_6
+#define MOTOR_BIN2_GPIO_Port GPIOE
+#define ULTRASONIC_ECHO_Pin GPIO_PIN_2
+#define ULTRASONIC_ECHO_GPIO_Port GPIOA
+#define ULTRASONIC_TRIG_Pin GPIO_PIN_3
+#define ULTRASONIC_TRIG_GPIO_Port GPIOA
+#define LED3_Pin GPIO_PIN_8
+#define LED3_GPIO_Port GPIOE
+#define IMU_INT_Pin GPIO_PIN_12
+#define IMU_INT_GPIO_Port GPIOB
+#define SERVO_TIM12_CH2_Pin GPIO_PIN_15
+#define SERVO_TIM12_CH2_GPIO_Port GPIOB
+#define OLED_DC_Pin GPIO_PIN_11
+#define OLED_DC_GPIO_Port GPIOD
+#define OLED_RESET__Pin GPIO_PIN_12
+#define OLED_RESET__GPIO_Port GPIOD
+#define OLED_SDIN_Pin GPIO_PIN_13
+#define OLED_SDIN_GPIO_Port GPIOD
+#define OLED_SCLK_Pin GPIO_PIN_14
+#define OLED_SCLK_GPIO_Port GPIOD
+#define MOTOR_AIN2_Pin GPIO_PIN_8
+#define MOTOR_AIN2_GPIO_Port GPIOB
+#define MOTOR_AIN1_Pin GPIO_PIN_9
+#define MOTOR_AIN1_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
